098cda6b8cbbf052f20aab5d9f25e87828cd0cc6
mdrony5134 <rony830388@gmail.com>
2024-01-27 18:17:38 +0600

    navbar and banner section done

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let nav = document.querySelector(".navbar");
+window.scroll = function(){
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("header-scrolled");
+  }else{
+    nav.classList.remove("header-scrolled")
+  }
+}
+// nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach((a)=>{
+  a.addEventListener("click",()=>{
+    navCollapse.classList.remove("show");
+  })
+})
+
+// owl carousel
+$(document).ready(function(){
+ $(".client-slider-section").owlCarousel({
+  items: 4,
+  loop: true,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsiveClass: true,
+  responsive: {
+      0: {
+          items: 2
+      },
+      600: {
+          items: 3
+      },
+      1000: {
+          items: 6
+      }
+  }
+ });
+});
